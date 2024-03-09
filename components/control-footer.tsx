377553ad90a2c6e1f8d10a720340c9b4647d0233
Daniel Mellado <mellado.m2@gmail.com>
2024-03-09 14:33:51 +0100

    Add hover effect to control buttons

--- components/control-footer.tsx
+++ components/control-footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { FlipSvg } from "./svgs/flipSvg";
 import { NextSvg } from "./svgs/nextSvg";
 
@@ -10,24 +12,40 @@ export function ControlFooter({
   handleNextCard,
   handleFlip,
 }: ControlFooterProps) {
+  const [hover, setHover] = useState<string>("");
+
   return (
-    <footer className="text-white mt-10 flex gap-9 justify-center">
-      <button
-        className="transition hover:scale-110 duration-300 ease-in-out"
-        onClick={handleNextCard}
-      >
-        <div className="bg-white rounded-full block p-4">
-          <NextSvg />
+    <footer className="grid gap-10 ">
+      <div className="text-white mt-10 flex gap-9 justify-center">
+        <button
+          className="transition hover:scale-110 duration-300 ease-in-out"
+          onClick={handleNextCard}
+          onMouseEnter={() => setHover("next")}
+          onMouseLeave={() => setHover("")}
+        >
+          <div className="bg-white rounded-full block p-4">
+            <NextSvg />
+          </div>
+        </button>
+        <button
+          className="transition hover:scale-110 duration-300 ease-in-out"
+          onClick={handleFlip}
+          onMouseEnter={() => setHover("flip")}
+          onMouseLeave={() => setHover("")}
+        >
+          <div className="bg-white rounded-full block p-4">
+            <FlipSvg />
+          </div>
+        </button>
+      </div>
+
+      {hover && (
+        <div>
+          <p className="uppercase font-bold underline text-center text-2xl">
+            {hover}
+          </p>
         </div>
-      </button>
-      <button
-        onClick={handleFlip}
-        className="transition hover:scale-110 duration-300 ease-in-out"
-      >
-        <div className="bg-white rounded-full block p-4">
-          <FlipSvg />
-        </div>
-      </button>
+      )}
     </footer>
   );
 }
