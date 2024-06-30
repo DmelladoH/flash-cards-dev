1d4e4b98b1d961a0fa128c02ab71b1d520e92f83
Daniel Mellado <mellado.m2@gmail.com>
2024-06-30 19:11:41 +0200

    feat: remove unused conde

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { startDrag } from "~/helpers/dragHelper";
 
 export function useDrag() {
-  const [draggableCard, setDraggableCard] = useState<HTMLElement>();
-  const [backgroundCard, setBackgroundCard] = useState<HTMLElement>();
-
   const [isDragging, setIsDragging] = useState(false);
   const [mouseDownPosition, setMouseDownPosition] = useState<{
     x: number;
@@ -38,13 +35,6 @@ export function useDrag() {
     }
   };
 
-  useEffect(() => {
-    setDraggableCard(document.querySelector(".draggable") as HTMLElement);
-    setBackgroundCard(
-      document.getElementsByClassName("first").item(0) as HTMLElement,
-    );
-  }, []);
-
   const dragEvent = (e: MouseEvent | TouchEvent, next: () => void) => {
     startDrag({
       isAnimating,
@@ -53,27 +43,7 @@ export function useDrag() {
     });
   };
 
-  const handleNextCard = (action: () => void) => {
-    if (!draggableCard) return;
-    draggableCard.style.transition = "transform 1s";
-    draggableCard.style.transform = `translateX(${2000}px) rotate(${90}deg)`;
-
-    if (backgroundCard) {
-      backgroundCard?.classList.add("detransform1");
-    }
-
-    draggableCard.addEventListener(
-      "transitionend",
-      () => {
-        isAnimating = false;
-        action();
-      },
-      { once: true },
-    );
-  };
-
   return {
-    handleNextCard,
     dragEvent,
     handleMouseDown,
     handleMouseUp,
