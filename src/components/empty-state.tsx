01226e849defe5248a039a9c03b9e7e12ca74521
Daniel Mellado <mellado.m2@gmail.com>
2024-07-21 15:33:00 +0200

    add go back to menu button on the empty state screen

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 
 export function EmptyState({ category }: { category: string }) {
   return (
-    <div className="mt-20 grid gap-10">
-      <p>There are no more cards.</p>
+    <div className="mt-20 grid justify-center gap-10 align-middle">
+      <p className="text-center">There are no more cards.</p>
       <Link
         href={`/${category}`}
-        className="rounded-lg bg-slate-50 p-1 text-center uppercase text-black"
+        className="rounded-lg bg-slate-50 p-2 text-center uppercase text-black"
       >
         reset
+      </Link>
+      <Link
+        href={"/"}
+        className="border-bg-slate-50 text-bg-slate-50 rounded-lg border-2 border-solid p-2 text-center uppercase"
+      >
+        choose another category
       </Link>
     </div>
   );
