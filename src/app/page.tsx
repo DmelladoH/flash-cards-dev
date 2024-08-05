37b723e231db536cdfbe3785a92653ad7d5857de
Daniel Mellado <mellado.m2@gmail.com>
2024-08-05 20:12:54 +0200

    feat: center category section title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,8 @@ export default async function HomePage() {
   return (
     <main className="grid h-screen grid-rows-3">
       <HeroSection />
-      <div className="relative row-span-2 ">
+      <div className="relative row-span-2">
         <CategorySection />
-        {/* <div className="m-x-2 absolute bottom-0 z-30 h-60 w-full bg-slate-600"></div> */}
       </div>
     </main>
   );
--- src/components/category-section.tsx
+++ src/components/category-section.tsx
@@ -4,8 +4,8 @@ import CategoryCard from "./UI/category-card";
 export function CategorySection() {
   return (
     <>
-      <section className="absolute z-20 mx-8 h-full w-[-webkit-fill-available] rounded-t-3xl border-2 bg-white p-10 text-stone-900">
-        <h2>Choose the category</h2>
+      <section className="absolute z-20 mx-8 h-full w-[-webkit-fill-available] rounded-t-3xl border-2 bg-white p-12 text-stone-900">
+        <h2 className="text-center text-lg font-medium">Choose the category</h2>
         <ul className="gallery mt-8">
           <li>
             <CategoryCard
