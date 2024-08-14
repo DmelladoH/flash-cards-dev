408f860ed7fb5bbfbea0c0ce969b42196dd0971f
Daniel Mellado <mellado.m2@gmail.com>
2024-08-14 18:49:28 +0200

    feat: add responsive text

--- src/components/category-section.tsx
+++ src/components/category-section.tsx
@@ -4,8 +4,10 @@ import CategoryCard from "./UI/category-card";
 export function CategorySection() {
   return (
     <>
-      <section className="absolute z-20 mx-8 h-full w-[-webkit-fill-available] rounded-t-3xl border-2 bg-white p-12 text-stone-900">
-        <h2 className="text-center text-lg font-medium">Choose the category</h2>
+      <section className="absolute z-20 mx-2 h-full w-[-webkit-fill-available] rounded-t-3xl border-2 bg-white p-12 text-stone-900 md:mx-8">
+        <h2 className="text-center text-lg font-medium md:text-xl">
+          Choose the category
+        </h2>
         <ul className="gallery mt-8">
           <li>
             <CategoryCard
@@ -16,7 +18,7 @@ export function CategorySection() {
           </li>
         </ul>
       </section>
-      <div className="transform-bg-card absolute left-0 right-0 top-0 -z-10 mx-7 h-[500px] rounded-t-3xl rounded-br-[21.5rem] border-2 bg-white p-10"></div>{" "}
+      <div className="transform-bg-card absolute left-0 right-0 top-0 -z-10 mx-1 h-[500px] rounded-t-3xl rounded-br-[21.5rem] border-2 bg-white p-10 md:mx-7"></div>{" "}
     </>
   );
 }
--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,7 +1,9 @@
 export function HeroSection() {
   return (
-    <section className="p-16">
-      <h1 className=" font-paytone text-center text-7xl">FlashCards</h1>
+    <section className="pt-20">
+      <h1 className=" text-center font-paytone text-5xl md:text-7xl">
+        FlashCards
+      </h1>
     </section>
   );
 }
