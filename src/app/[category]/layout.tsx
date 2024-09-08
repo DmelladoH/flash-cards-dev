6c549e08265e9a6403afdad6f615834fe8525bda
Daniel Mellado <mellado.m2@gmail.com>
2024-09-08 14:20:24 +0200

    fix: remove deck provider

--- src/app/[category]/layout.tsx
+++ src/app/[category]/layout.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { ControlFooter } from "~/components/control-footer";
 import BackSvg from "~/components/svgs/backSvg";
 import { DeckProvider } from "~/context/deck-context";
-import { DragProvider } from "~/context/drag-context";
 
 export default function CategoryLayout({
   children,
@@ -13,20 +12,18 @@ export default function CategoryLayout({
 }) {
   return (
     <DeckProvider>
-      <DragProvider>
-        <div className="flashcards-layout">
-          <header className="flex justify-center p-7 align-middle">
-            <Link href={"/"} className="flex items-center">
-              <BackSvg />
-            </Link>
-            <div className="">
-              <h1 className="text-3xl">{category}</h1>
-            </div>
-          </header>
-          <main className="flex items-start justify-center">{children}</main>
-          <ControlFooter />
-        </div>
-      </DragProvider>
+      <div className="flashcards-layout">
+        <header className="flex justify-center p-7 align-middle">
+          <Link href={"/"} className="flex items-center">
+            <BackSvg />
+          </Link>
+          <div className="">
+            <h1 className="text-3xl">{category}</h1>
+          </div>
+        </header>
+        <main className="flex items-start justify-center">{children}</main>
+        <ControlFooter />
+      </div>
     </DeckProvider>
   );
 }
--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,23 +1,16 @@
 "use client";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
-import { useDeck } from "~/hooks/useDeck";
 import { useDeckContext } from "~/hooks/useDeckContext";
 
 function Page({ params }: { params: { category: string } }) {
   const { category } = params;
-
-  // const { currentCard, error, isLoading } = useDeck({ category });
 
   const { deck, isLoading, fetchData } = useDeckContext();
 
   useEffect(() => {
     fetchData({ category });
   }, []);
-
-  // if (error != null) {
-  //   return <div>404: card wasn't found </div>;
-  // }
 
   if (isLoading) {
     return <p>loading...</p>;
