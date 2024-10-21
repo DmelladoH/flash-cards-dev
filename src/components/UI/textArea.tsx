22f23321abd0d38b8d9f4ceeeb1829b89e910b4d
Daniel Mellado <mellado.m2@gmail.com>
2024-10-21 20:11:51 +0200

    feat: add text area component

---
+++ src/components/UI/textArea.tsx
@@ -0,0 +1,18 @@
+interface TextAreaProps {
+  label: string;
+  name: string;
+  placeholder: string;
+}
+
+export default function TextArea({ label, name, placeholder }: TextAreaProps) {
+  return (
+    <label className="grid text-lg">
+      {label}
+      <textarea
+        name={name}
+        placeholder={placeholder}
+        className="min-h-60 rounded-lg px-4 py-2 text-sm"
+      />
+    </label>
+  );
+}
