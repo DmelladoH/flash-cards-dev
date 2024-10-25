eea62b9ed2a50650d9fb74082b06cfbfba0ef4f4
Daniel Mellado <mellado.m2@gmail.com>
2024-10-25 15:33:20 +0200

    feat: refactor input

--- src/components/UI/input.tsx
+++ src/components/UI/input.tsx
@@ -1,16 +1,22 @@
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   name: string;
-  placeholder: string;
+  placeholder?: string;
 }
-export default function Input({ label, name, placeholder }: InputProps) {
+export default function Input({
+  label,
+  name,
+  placeholder,
+  ...props
+}: InputProps) {
   return (
     <label className="grid text-lg">
       {label}
       <input
         name={name}
         placeholder={placeholder}
-        className="rounded-lg px-4 py-2 text-sm"
+        className="rounded-lg px-4 py-2 text-sm text-black"
+        {...props}
       />
     </label>
   );
