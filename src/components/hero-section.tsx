1a661a67d5bf19647c14b4760c080c0569ae8c61
Daniel Mellado <mellado.m2@gmail.com>
2024-10-26 17:00:53 +0200

    fix: px

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -4,7 +4,7 @@ export function HeroSection() {
   return (
     <section className="md:py-20">
       <div className="flex h-56 items-center justify-center">
-        <LogoSvg className="h-[200] w-full md:h-[300]" />
+        <LogoSvg className="h-[200px] md:h-[300px]" />
       </div>
     </section>
   );
