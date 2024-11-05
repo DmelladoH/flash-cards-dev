3f0c87486790a97015ef7b56c13b6bb6e63e6ad4
Daniel Mellado <mellado.m2@gmail.com>
2024-11-05 19:27:12 +0100

    feat: improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,31 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "CardsFlash",
-  description: "Flash cards for devs",
+  title: "CardsFlash - Flashcards for Developers",
+  description:
+    "CardsFlash is a flashcard app designed to help developers and tech enthusiasts learn and retain programming knowledge efficiently. Improve your coding skills with customizable flashcards covering various programming languages, frameworks, and concepts.",
+  keywords:
+    "flashcards, developers, programming, coding, learning, app, tech, study, education, CardsFlash, software development, coding skills, React, Performance, web",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  // openGraph: {
+  //   title: "CardsFlash - Flashcards for Developers",
+  //   description: "Boost your coding skills with CardsFlash, a flashcard app for developers covering a range of programming languages and concepts.",
+  //   // images: [
+  //   //   {
+  //   //     url: "/images/cardsflash-og.png",
+  //   //     width: 1200,
+  //   //     height: 630,
+  //   //     alt: "CardsFlash - Flashcards for Developers",
+  //   //   },
+  //   // ],
+  // },
+  twitter: {
+    card: "summary_large_image",
+    title: "CardsFlash - Flashcards for Developers",
+    description:
+      "Learn coding concepts efficiently with CardsFlash. Customizable flashcards for devs to strengthen programming knowledge.",
+    // images: ["/images/cardsflash-og.png"],
+  },
 };
 
 export default function RootLayout({
