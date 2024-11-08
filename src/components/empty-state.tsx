42c85c27224e9894309bb373fe227ba04586ee59
Daniel Mellado <mellado.m2@gmail.com>
2024-11-08 21:05:48 +0100

    feat: add secondary button

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -3,17 +3,19 @@ import CallToAction from "./UI/buttons/cta";
 
 export function EmptyState({ category }: { category: string }) {
   return (
-    <div className="mt-20 grid justify-center gap-10 align-middle ">
-      <p className="text-center">Having fun?</p>
-      <CallToAction actionType="link" href={`/${category}`}>
-        Get more
-      </CallToAction>
-      <Link
-        href={"/"}
-        className="border-bg-slate-50 text-bg-slate-50 rounded-lg border-2 border-solid p-2 text-center uppercase"
-      >
-        choose another category
-      </Link>
+    <div className="grid justify-center">
+      <div className="mt-32 grid w-60 justify-center gap-10 align-middle">
+        <p className="text-center">Having fun?</p>
+        <CallToAction actionType="link" href={`/${category}`}>
+          Get more
+        </CallToAction>
+        <Link
+          href={"/"}
+          className="w-full rounded-lg border-2 border-solid border-white p-2 text-center font-paytone text-white shadow-[0_4px_0_#ffffff] hover:bg-[rgba(255,255,255,0.2)] active:translate-y-1 active:shadow-none"
+        >
+          Choose another category
+        </Link>
+      </div>
     </div>
   );
 }
