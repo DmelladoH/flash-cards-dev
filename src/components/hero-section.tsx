9ce1b93bb1268e800e328f9b562f70588ac8ae9b
Daniel Mellado <mellado.m2@gmail.com>
2024-11-09 10:44:10 +0100

    fix: heroSection

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -2,7 +2,7 @@ import LogoSvg from "./svgs/logo";
 
 export function HeroSection() {
   return (
-    <section className="flex items-center justify-center py-10">
+    <section>
       <div className="flex items-center justify-center">
         <LogoSvg className="h-[150px] w-[400px] md:h-[300px] md:w-[600px]" />
       </div>
