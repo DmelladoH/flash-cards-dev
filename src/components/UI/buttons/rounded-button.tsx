3dc1bed5bcb063d801bb497910a3205370d2fc9d
Daniel Mellado <mellado.m2@gmail.com>
2024-11-10 20:34:06 +0100

    feat: change radiobutton style

--- src/components/UI/buttons/rounded-button.tsx
+++ src/components/UI/buttons/rounded-button.tsx
@@ -6,10 +6,10 @@ function RoundedButton({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className="transition duration-300 ease-in-out hover:scale-110"
+      className="block rounded-full bg-white  p-4 shadow-[0_4px_0_#ffffff7a] hover:bg-[rgba(255,255,255,0.8)] active:translate-y-1 active:shadow-none"
       {...props}
     >
-      <div className="block rounded-full bg-white p-4">{children}</div>
+      {children}
     </button>
   );
 }
